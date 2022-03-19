1b5d7dd91f39b83f474a70c8cab311d2d5c98c03
swang1212 <99310286+swang1212@users.noreply.github.com>
2022-03-19 00:31:52 -0700

    Update Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,14 +1,68 @@
 import React from "react";
 import './styles/Board.css';
 import Square from "./Square";
+import { useState } from 'react'
+
 
 function Board() {
-
-    const status = 'Next player: X';
+  const [squares, setSquares ] = useState(Array(9).fill(null)); 
+  const [NextPlayer, setNextPlayer] = useState("X")
+  const winner = findWinner(squares);
 
     function renderSquare(i) {
-        return <Square />;
+        return <Square squareValue = {squares[i]} Clicked={()=>{
+          if(winner) {
+              return
+            }
+          if(squares[i]) {
+            return
+          }
+
+          const newSquares = squares.slice();
+          newSquares[i] = NextPlayer
+          setSquares(newSquares)
+
+          if(NextPlayer == "X") {
+            setNextPlayer("O")
+          } else {
+            setNextPlayer("X")
+          }
+  
+        }}/>;
     }
+
+    function findWinner(squares) {
+      const winningCombos = [
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ];
+      for (let i = 0; i < 2; i++) {
+        const [j, k, l] = winningCombos[i];
+        if (squares[j] && squares[j] === squares[k] && squares[j] === squares[l]) {
+          return squares[j];
+        }
+      }
+      return null;
+    }
+
+    function GameStatus() {
+      if(winner) {
+        if(NextPlayer == "X") {
+          return "O is the winner!"
+        } else {
+          return "X is the winner!"
+        }
+      } else {
+        return "Next Player: " + NextPlayer
+      }
+    }
+    
 
     return (  
         <div>
@@ -27,7 +81,9 @@ function Board() {
             {renderSquare(7)}
             {renderSquare(8)}
           </div>
-          <div className="status">{status}</div>
+          <div className="status">
+            <p>{GameStatus()}</p>
+            </div>
         </div>
     )
 }
