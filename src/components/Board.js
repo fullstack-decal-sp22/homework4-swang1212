df33a1db25d747648f4df758ef01a000e49063ac
swang1212 <99310286+swang1212@users.noreply.github.com>
2022-03-19 00:49:48 -0700

    Update Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -33,16 +33,16 @@ function Board() {
 
     function findWinner(squares) {
       const winningCombos = [
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
         [0, 4, 8],
         [2, 4, 6]
       ];
-      for (let i = 0; i < 2; i++) {
+      for (let i = 0; i < winningCombos.length; i++) {
         const [j, k, l] = winningCombos[i];
         if (squares[j] && squares[j] === squares[k] && squares[j] === squares[l]) {
           return squares[j];
